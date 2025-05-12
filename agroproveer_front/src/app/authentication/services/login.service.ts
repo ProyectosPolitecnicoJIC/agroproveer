ce387efe04340c2b5c7e0bc1f7962a6eeb26c030
geffrys <geffry.ospina@gmail.com>
2025-05-12 00:53:25 -0500

    Pequeña correccion en el servicio de login

--- agroproveer_front/src/app/authentication/services/login.service.ts
+++ agroproveer_front/src/app/authentication/services/login.service.ts
@@ -10,14 +10,13 @@ import { Login } from "../../models/login.interface";
 })
 export class LoginService {
 
-    private apiUrl = `https://29cbed90-c872-430d-bf89-f1bdf67eca6c.mock.pstmn.io/login`;
+    private apiUrl = `http://localhost:8096/auth/login`;
 
     constructor(private http: HttpClient) { }
 
-    login(username: string, password: string): Observable<boolean> {
-        const body: Login = { username, password };
-        return this.http.post<boolean>(this.apiUrl, body).pipe(
-            map(response => response ? true : false), 
+    login(user: Login): Observable<boolean> {
+        return this.http.post<Login>(this.apiUrl, user).pipe(
+            map(response => response ? true : false),
             catchError(error => {
                 console.error('Login error', error);
                 return of(false);
